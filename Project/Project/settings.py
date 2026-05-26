0e908ece05fbf4a0dfa782bd91982ff6a1d27a43
Sara Jara <sajara154@gmail.com>
2026-05-26 10:39:25 -0300

    DB

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
 TIME_ZONE = 'UTC'
 
--- Project/api/models.py
+++ Project/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class alumnos(Models.Model):
+    id_alumnos=autoField(primary_key=true)
+    dni=models.TextField(max_length=10)
+    apellido=models.TextField(max_length=35)
+    nombre=models.TextField(max_length=35)
+    edad=models.IntegerField()
+    calle=models.TextField(max_length=50)
+    altuta=models.IntegerField()
+
+    def __str__(self):
+        return self.dni
